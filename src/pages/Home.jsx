627248be53f6dcaec7444363f1bf3134a264e874
Melissa Domingos <melissagomes@eaportal.org>
2024-06-10 03:51:48 +0000

    style: estilização completa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import { ButtonContainer, CardContainer, IconContainer } from "../styles/Home.styles";
+import { ButtonContainer, CardContainer, IconContainer, ResultContainer } from "../styles/Home.styles";
 
 import iconStarImg from "../assets/icon-star.svg"
+import thankYouImg from "../assets/illustration-thank-you.svg"
 
 export function Home() {
-    return (
+  let aparecerResultado = true
+
+  return (
+    aparecerResultado === false ? (
       <CardContainer>
         <IconContainer>
           <img src={iconStarImg} alt="ícone de estrela" />
@@ -22,6 +26,19 @@ export function Home() {
 
           <button>Enviar</button>
       </CardContainer>
+      ) : (
+        <CardContainer>
+          <img src={thankYouImg} alt="imagem de agradecimento" />
+
+          <ResultContainer>
+            <p>Voce selecionou 4 de 5</p>
+          </ResultContainer>
+
+          <h1>Obrigado!</h1>
+
+          <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato!</p>
+      </CardContainer>
+      )
     )
   }
   
